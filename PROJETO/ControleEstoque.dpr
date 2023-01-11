0efe2cbb6fe17d9beb07df3f9bfb8fcaa7cf9022
DESKTOP-22BMFRD\atere <aterezinha.19@hotmail.com>
2023-01-10 22:39:50 -0300

    Criar tela do Menu Principal

--- PROJETO/ControleEstoque.dpr
+++ PROJETO/ControleEstoque.dpr
@@ -2,13 +2,13 @@ program ControleEstoque;
 
 uses
   Vcl.Forms,
-  UnitPrincipal in '..\FORMS\UnitPrincipal.pas' {Form1};
+  UnitPrincipal in '..\FORMS\UnitPrincipal.pas' {Form_Principal};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TForm_Principal, Form_Principal);
   Application.Run;
 end.
